ea72a1d3c2ecf860d2982f11ddbea46f7ac97d33
Laeliyan <valette.clara@gmail.com>
2019-01-20 14:54:51 +0100

    suppression all-in-one

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyCFBCb9LIEtCOImC0gQ4W4UikmFjHH2YKQ",
+    authDomain: "vocatrainerfirebase.firebaseapp.com",
+    databaseURL: "https://vocatrainerfirebase.firebaseio.com",
+    storageBucket: "vocatrainerfirebase.appspot.com",
+    messagingSenderId: "30374019027"
+};
+
